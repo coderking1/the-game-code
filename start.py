01f36a30e42d58497e6dc2f7764a39d09088f2ed
lawrence <zhanghechen2@gmail.com>
2018-03-10 12:21:39 +0800

    Update start.py

--- start.py
+++ start.py
@@ -1,5 +1,8 @@
 import packages.py
 app = Flask(__name__)
-@app.route("/")
-def hello():
+@app.route('/')
+def index():
+    return 'this is the starting page'
+@app.route("/start")
+def start():
     return "the kings rise again -- you ride to save your republic!"
